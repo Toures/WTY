6b93a271c6aec03ba1181271b77c15c9a15508f5
Toures <Mike.Hukiewitz@gmx.de>
2015-07-15 11:58:23 +0200

    + levelData() + Timer für levels

--- js/game.js
+++ js/game.js
@@ -3,15 +3,17 @@ window.onload = function(){
 };
 
 function dogtoaster(){
-    var HEIGHT = 600;
-    var WIDTH = 800;
-    var percent = (1.0/100.0);
     var game = new Phaser.Game(WIDTH, HEIGHT, Phaser.AUTO, '',{preload:preload, create:create, update:update});
+    //Dimensionen des Spielfeldes (sollte ein Vielfaches der tilesize sein)
+    var tilesize = 64; //Groesse der Felder
+    var HEIGHT = (10*tilesize);
+    var WIDTH = (14*tilesize);
+    var timer, time; //Zeitanzeige
+    var keys; //Tasteneingabe
+    //Laserobjekte
     var graLines;
-
-    var keys;
     var geoLines = [];
-    var objects = [];
+    var objects = [];//Alle Spielobjekte welche mit den Lasern interagieren
 
     function preload(){
         keys = {
@@ -27,12 +29,20 @@ function dogtoaster(){
         graLines = game.add.graphics(0,0);
         graLines.lineStyle(3,0xff0000,1);
         addLaser(300,300,400,300);
-        console.log(geoLines[0].start.x + " " + geoLines[0].start.y);
-        console.log(geoLines[0].end.x + " " + geoLines[0].end.y);
+        castLaser(400,300,"oben");
+
+        time = 11;
+        timer = game.add.text(20,20,time+"", {font: '15px Arial', fill:'#ffffff', align: 'center'});
     }
 
     function update() {
         dt = game.time.elapsed;
+        //Countdown, wenn abgelaufen -> GameOver bzw. Neustart des Levels
+        time -= dt/1000;
+        timer.text = Phaser.Math.truncate(time)+"";
+        if(Phaser.Math.truncate(time) <= 0) {
+            gameOver();
+        }
 
         if (keys.up.isDown) {
             console.log(geoLines.length + "");
@@ -60,9 +70,10 @@ function dogtoaster(){
         var xTo = 0, yTo = 0;
         switch (dir) {
             case "oben":
-                for (; inside(y + yTo, 0, HEIGHT); yTo += tilesize) {
+                for (; inside(y + yTo, 0, HEIGHT); yTo -= tilesize) {
+                    console.log(yTo+"");
                     for (var i in objects) {
-                        if (objects[i].sprite.x === x + xTo && objects[i].sprite.y === y + yTo) {
+                        if (Phaser.Math.within(objects[i].sprite.x,x+xTo,10) && Phaser.Math.within(objects[i].sprite.y,y+yTo,10)) {
                             objects[i].laserHit(dir);
                             break;
                         }
@@ -70,9 +81,9 @@ function dogtoaster(){
                 }
                 break;
             case "unten":
-                for (; inside(y + yTo, 0, HEIGHT); yTo -= tilesize) {
+                for (; inside(y + yTo, 0, HEIGHT); yTo += tilesize) {
                     for (var i in objects) {
-                        if (objects[i].sprite.x === x + xTo && objects[i].sprite.y === y + yTo) {
+                        if (Phaser.Math.within(objects[i].sprite.x,x+xTo,10) && Phaser.Math.within(objects[i].sprite.y,y+yTo,10)) {
                             objects[i].laserHit(dir);
                             break;
                         }
@@ -82,7 +93,7 @@ function dogtoaster(){
             case "links":
                 for (; inside(x + xTo, 0, WIDTH); xTo -= tilesize) {
                     for (var i in objects) {
-                        if (objects[i].sprite.x === x + xTo && objects[i].sprite.y === y + yTo) {
+                        if (Phaser.Math.within(objects[i].sprite.x,x+xTo,10) && Phaser.Math.within(objects[i].sprite.y,y+yTo,10)) {
                             objects[i].laserHit(dir);
                             break;
                         }
@@ -90,9 +101,9 @@ function dogtoaster(){
                 }
                 break;
             case "rechts":
-                for (; inside(x + xTo, 0, WIDTH); xTo -= tilesize) {
+                for (; inside(x + xTo, 0, WIDTH); xTo += tilesize) {
                     for (var i in objects) {
-                        if (objects[i].sprite.x === x + xTo && objects[i].sprite.y === y + yTo) {
+                        if (Phaser.Math.within(objects[i].sprite.x,x+xTo,10) && Phaser.Math.within(objects[i].sprite.y,y+yTo,10)) {
                             objects[i].laserHit(dir);
                             break;
                         }
@@ -100,14 +111,16 @@ function dogtoaster(){
                 }
                 break;
             default:
+                console.log("Und castLaser() so: \"NOPE\"");
                 return null;
         }
 
         var child = game.add.graphics(0,0);
+        child.lineStyle(3,0xff0000,1);
         child.moveTo(x,y);
         child.lineTo(x+xTo,y+yTo);
         graLines.addChild(child);
-        geoLines.push(new Line(x, y, x + xTo, y + yTo));
+        geoLines.push(new Phaser.Line(x, y, x + xTo, y + yTo));
     }
 
     //Entfernt alle Laserobjekte ab einem bestimmten Laserobjekt (d.h. alle darauffolgende Laserobjekte)
@@ -117,7 +130,44 @@ function dogtoaster(){
         geoLines.splice(geoLines.indexOf(laser),geoLines.length-geoLines.indexOf(laser));
     }
 
+    //Bei uebergabe der Level wird ein Levelobjekt zurückgegeben mit allen Informationen über das Level
+    //In default ist eine Vorlage für ein Level, kopiert diese einfach in die verschiedenen cases für
+    //die verschiedenen Level.
+    function levelData(levelNumber){
+        var level;
+        switch (levelNumber) {
+            case 1:
+                break;
+            case 2:
+                break;
+            //etc. etc.
+            default:
+                level = {
+                    time: 10,
+                    posDogX: 2 * tilesize,
+                    posDogY: 2 * tilesize,
+                    posLaserX: 5 * tilesize,
+                    posLaserY: 0,
+                    dirLaser: "unten",
+                    generateObjects: function () {
+                        //Hier wird alles zum setup des levels gemacht, unter anderem:
+                        //Spielobjekte erschaffen
+                    },
+                    postInit: function() {
+                        //Hier können nachdem das Level initialisiert wurde weitere Einstellungen
+                        //vorgenommen werden, z.b. spawnrate der Zeitpacks(Batterien) geändert werden
+                    }
+                }
+        }
+
+        return level;
+    }
+
     function inside(i, a, b) {
         return ((i >= a) && (i <= b));
+    }
+
+    function gameOver() {
+
     }
 }
